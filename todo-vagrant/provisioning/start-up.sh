680af70b89fc06fb1c2765e3a59aa1fc28a0127d
Jason Felege <jason@jasonfelege.com>
2016-08-18 09:50:02 -0500

    start gradlew daemon, and application on vagrant up

---
+++ todo-vagrant/provisioning/start-up.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+echo "start up script"
+
+# force gradle daemon to always run
+[[ -d ~/.gradle ]] || mkdir ~/.gradle
+touch ~/.gradle/gradle.properties && echo "org.gradle.daemon=true" >> ~/.gradle/gradle.properties
+
+# switch cwd to todo-app folder
+cd /host_todo-app
+
+./gradlew assemble
+
+# execute the application
+nohup java -Dspring.profiles.active=production -jar ./build/libs/*.jar 0<&- &>/dev/null &
