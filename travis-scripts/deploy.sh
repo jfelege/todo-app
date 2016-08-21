b45715a6b8ce704d9ab0020474eed0d703b16261
Jason Felege <jason@jasonfelege.com>
2016-08-21 16:47:10 -0500

    travis: fix paths, add logging statements

--- travis-scripts/deploy.sh
+++ travis-scripts/deploy.sh
@@ -2,11 +2,19 @@
 
 DO_HOST=159.203.96.110
 
+echo "TRAVIS_BUILD_DIR => ${TRAVIS_BUILD_DIR}"
+
+echo "MKDIR on remote"
 ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa root@${DO_HOST} 'mkdir -p /host_todo-app/build/asciidoc'
 ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa root@${DO_HOST} 'mkdir -p /host_todo-app/build/libs'
 ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa root@${DO_HOST} 'mkdir -p /host_docker-nginx'
 
-scp -oStrictHostKeyChecking=no -r /host_docker-nginx root@${DO_HOST}:/host_docker-nginx
-scp -oStrictHostKeyChecking=no -r /host_todo-app/build/asciidoc root@${DO_HOST}:/host_todo-app/build/asciidoc
-scp -oStrictHostKeyChecking=no -r /host_todo-app/build/libs root@${DO_HOST}:/host_todo-app/build/libs
+echo "UPLOAD nginx config"
+scp -oStrictHostKeyChecking=no -r ${TRAVIS_BUILD_DIR}/todo-vagrant/docker-nginx root@${DO_HOST}:/host_docker-nginx
+
+echo "UPLOAD asciidoc"
+scp -oStrictHostKeyChecking=no -r ${TRAVIS_BUILD_DIR}/todo-app/build/asciidoc root@${DO_HOST}:/host_todo-app/build/asciidoc
+
+echo "UPLOAD libs"
+scp -oStrictHostKeyChecking=no -r ${TRAVIS_BUILD_DIR}/todo-app/build/libs root@${DO_HOST}:/host_todo-app/build/libs
 
