87bfe1d14c94885a09c4b97c25b24e95bd3b6ac8
Jason Felege <jason@jasonfelege.com>
2016-08-20 22:39:55 -0500

    removed unused imports

--- todo-app/src/main/java/com/jasonfelege/todo/configuration/WebAppConfig.java
+++ todo-app/src/main/java/com/jasonfelege/todo/configuration/WebAppConfig.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
