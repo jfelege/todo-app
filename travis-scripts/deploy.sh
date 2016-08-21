7e4e897f8ff26cf3419d6fa6a2cd7947138eb06c
Jason Felege <jason@jasonfelege.com>
2016-08-21 16:31:48 -0500

    travis: update deploy script

--- travis-scripts/deploy.sh
+++ travis-scripts/deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo ${TRAVIS_BUILD_DIR}
+DO_HOST=159.203.96.110
 
-ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa root@159.203.96.110 'mkdir -p /host_todo-app/build/asciidoc'
-ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa root@159.203.96.110 'mkdir -p /host_todo-app/build/libs'
+ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa root@${DO_HOST} 'mkdir -p /host_todo-app/build/asciidoc'
+ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa root@${DO_HOST} 'mkdir -p /host_todo-app/build/libs'
+ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa root@${DO_HOST} 'mkdir -p /host_docker-nginx'
+
+scp -oStrictHostKeyChecking=no -r /host_docker-nginx root@${DO_HOST}:/host_docker-nginx
+scp -oStrictHostKeyChecking=no -r /host_todo-app/build/asciidoc root@${DO_HOST}:/host_todo-app/build/asciidoc
+scp -oStrictHostKeyChecking=no -r /host_todo-app/build/libs root@${DO_HOST}:/host_todo-app/build/libs
+
