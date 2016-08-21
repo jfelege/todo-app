81fcab929598b266713a1655ae578135762101a2
Jason Felege <jason@jasonfelege.com>
2016-08-21 16:17:53 -0500

    travis: use environment variable

--- travis-scripts/deploy.sh
+++ travis-scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-ssh -oStrictHostKeyChecking=no -i ./travis-scripts/id_rsa root@159.203.96.110 'mkdir -p /host_todo-app/build/asciidoc'
-ssh -oStrictHostKeyChecking=no -i ./travis-scripts/id_rsa root@159.203.96.110 'mkdir -p /host_todo-app/build/libs'
+echo ${TRAVIS_BUILD_DIR}
+
+ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa root@159.203.96.110 'mkdir -p /host_todo-app/build/asciidoc'
+ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa root@159.203.96.110 'mkdir -p /host_todo-app/build/libs'
