402faf6a6160c7abed39a35201bc70ec60e1b97e
Jason Felege <jason@jasonfelege.com>
2016-08-21 19:06:59 -0500

    travis: update deploy script to kill previous java process + re-execute springbok

--- travis-scripts/deploy.sh
+++ travis-scripts/deploy.sh
@@ -17,3 +17,9 @@ scp -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa -r $
 
 echo "UPLOAD libs"
 scp -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa -r ${TRAVIS_BUILD_DIR}/todo-app/build/libs root@${DO_HOST}:/host_todo-app/build
+
+echo "KILL springboot process"
+ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa root@${DO_HOST} 'pkill PIDNAME=todoapp'
+
+echo "START springboot process"
+ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/travis-scripts/id_rsa root@${DO_HOST} 'nohup java PIDNAME=todoapp -Dspring.profiles.active=production -jar /host_todo-app/build/libs/*.jar 0<&- &>/dev/null &'
